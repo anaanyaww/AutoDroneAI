90838b3ebe4ab813beb01d28c9f0d346fc1d3a9e
anaanyaww <ananyasirandass@gmail.com>
2025-01-18 21:50:15 +0530

    firebase changes

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDPwDzZD0WhYW3e6nquPfwELcllrH72vSg",
-  authDomain: "droneproject1-49b2f.firebaseapp.com",
-  projectId: "droneproject1-49b2f",
-  storageBucket: "droneproject1-49b2f.appspot.com",
+  apiKey: "yourkey",
+  authDomain: "yourkey",
+  projectId: "yourkey",
+  storageBucket: "yourkey",
   messagingSenderId: "689355327035",
-  appId: "1:689355327035:web:353f84d4ce1d1610f2fe1e",
-  measurementId: "G-6WKKBVDHYQ"
+  appId: "1yourkey0f2fe1e",
+  measurementId: "GyourkeyBVDHYQ"
 };
 
 // Initialize Firebase
